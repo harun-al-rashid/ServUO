5a8012af9f80bb1b7ae77e427b93ccc59561859f
unknown <andrewgs@gmail.com>
2021-06-01 13:44:51 +0930

    add fix to summoned daemon

--- Scripts/Mobiles/Summons/SummonedDaemon.cs
+++ Scripts/Mobiles/Summons/SummonedDaemon.cs
@@ -11,7 +11,7 @@ namespace Server.Mobiles
         public SummonedDaemon(int type)
             : base(AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4)
         {
-
+            m_type = type;
             switch (type)
             {
 
